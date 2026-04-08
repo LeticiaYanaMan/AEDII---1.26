81f19e305c3320b051adef8e7e991c03ed123a11
LeticiaYanaMan <ly.manca30@unifesp.br>
2026-04-08 00:54:13 -0300

    Create AP02com.c

---
+++ AP02com.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//definimos a variavel que vai contabilizar as quedas
+long long int quedas = 0; 
+
+void merge(int *vet, int comeco, int meio, int fim) {
+    // separa em dois tamanhos: um que contabiliza do começo ao meio, e outro do meio ao fim
+    printf("comeco: %d, meio: %d, fim: %d\n", comeco, meio, fim);
+    int tamcom = meio - comeco + 1;
+    printf("tamcom: %d\n", tamcom);
+    int tamfim = fim - meio;
+    printf("tamfim: %d\n", tamfim);
+
+    // cria vetores desses tamanhos que comportam dados de tamanho int
+    int *L = (int *)malloc(tamcom * sizeof(int));
+    int *R = (int *)malloc(tamfim * sizeof(int));
+
+    // preenche os vetores com os números devidos do vetor principal
+    printf("Vetor L: \n");
+    for (int i = 0; i < tamcom; i++) {
+        L[i] = vet[comeco + i];
+        printf("%d ", L[i]);
+    }
+    printf("\nVetor R: \n");
+    for (int i = 0; i < tamfim; i++) {
+        R[i] = vet[meio + 1 + i];
+        printf("%d ", R[i]);
+    }
+    printf("\n");
+
+    //criamos uma variavel auxiliar aux
+    int aux = 0;
+    for (int i = 0; i < tamfim; i++) {
+        while (aux < tamcom && L[aux] <= 2 * R[i]) //garantindo que o valor de aux será menor que a primeira metade e 
+        // que o primeiro vetor tem valor menor ou igual a 2 vezes o valor no segundo vetor 
+            aux++; // a variavel auxiliar vai somar 1 só se cumprir o fator que categoriza a queda
+        quedas += (tamcom - aux); // as quedas vaõ se aumentando conforme o tamanho da primeira metade - da variavel auxiliar
+    }
+    printf("aux: %d\n", aux);
+
+    int i = 0, j = 0, idx = comeco; //variaveis auxiliares
+    while (i < tamcom && j < tamfim) { 
+        if (L[i] <= R[j]) 
+            vet[idx++] = L[i++]; // se houver essa necessidade de troca, faz a troca que,
+        // no final, vai dar no vetor principal ordenado
+        else vet[idx++] = R[j++];
+    } 
+    printf("i: %d, j: %d, idx: %d\n", i, j, idx);
+    // agora vai substituir os valores no vetor principal, com os que precisavam já organizados
+    while (i < tamcom) 
+        vet[idx++] = L[i++];
+    printf("i final: %d\n", i);
+    while (j < tamfim) 
+        vet[idx++] = R[j++];
+    printf("j final: %d\n", j);
+
+    // libera os vetores auxiliares que criamos
+    free(L);
+    free(R);
+}
+
+void mergesort(int *vet, int comeco, int fim) {
+    printf("mergesort comeco: %d e fim: %d\n", comeco, fim);
+    if (comeco < fim) {
+        // vai dividindo o vetor de dois em dois até só ter 1 número armazenado em cada (comeco não vai ser < que fim)
+        int meio = comeco + (fim - comeco) / 2;
+        printf("mergesort meio: %d\n", meio);
+        mergesort(vet, comeco, meio);
+        mergesort(vet, meio + 1, fim);
+        // chama a função merge, que vai organizar cada vetor dividido
+        merge(vet, comeco, meio, fim);
+    }
+}
+
+int main(void) {
+    int N;
+    scanf("%d", &N); // número de dias
+    // cria um vetor desse tamanho
+    int *v = (int *)malloc(N * sizeof(int));
+    // vai ler essa quantidade N de números e armazena-los no vetor criado
+    for (int i = 0; i < N; i++) 
+        scanf("%d", &v[i]);
+    // vetor começa em 0 e termina em N-1
+    mergesort(v, 0, N - 1);
+
+    printf("%lld\n", quedas);
+    // imprime o vetor
+    for (int i = 0; i < N; i++) 
+        printf("%d ", v[i]);
+    printf("\n");
+
+    free(v); // libera o vetor
+    return 0;
+}
